906bd90ae8a92bfea253cf35e3a03ed645092942
handasha <64931178+handasha@users.noreply.github.com>
2024-11-30 12:32:00 -0800

    Add files via upload

---
+++ Registration/Registration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final_project.Views.Registration
+{
+    public class RegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
