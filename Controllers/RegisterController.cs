e7f3e6f8629adefd7f7161985892f0c504ca0943
SurajDesai1011 <97254880+SurajDesai1011@users.noreply.github.com>
2024-12-04 14:37:15 -0800

    Register

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Final_project.Models;
+using System.Threading.Tasks;
+
+namespace Final_project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public RegisterController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+    
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
--- obj/Debug/netcoreapp3.1/Final project.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Final project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb59f3d1b9da4cdb7a6af4e86dc0d80bbb1db7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe1de612d0582d64296ee72b54d7bd58be1cd9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
