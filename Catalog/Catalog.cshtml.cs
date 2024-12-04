76892a45bfcfb3248ed585c71a124f7436821bb6
handasha <64931178+handasha@users.noreply.github.com>
2024-12-04 15:23:35 -0800

    Add files via upload

---
+++ Catalog/Catalog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final_project.Views.Catalog
+{
+    public class CatalogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
