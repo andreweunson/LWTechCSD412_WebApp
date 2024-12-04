6fe1de612d0582d64296ee72b54d7bd58be1cd9c
SurajDesai1011 <97254880+SurajDesai1011@users.noreply.github.com>
2024-12-04 14:11:49 -0800

    loginpageController

--- Controllers/LoginpageController.cs
+++ Controllers/LoginpageController.cs
@@ -2,11 +2,35 @@
 
 namespace Final_project.Controllers
 {
-    public class LoginpageController
+    public class LoginpageController : Controller
     {
-       
-        
-    }
-    
-}
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                ViewBag.ErrorMessage = "Username and Password are required.";
+                return View();
+            }
+
+
+            TempData["Message"] = $"Welcome, {username}!";
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+
+            TempData["Message"] = "You have been logged out.";
+            return RedirectToAction("Login");
+        }
+    }
+}
