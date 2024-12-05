7ceebe21e91296e6e33b09cf5129c15ebf6f0ae3
Andrew Eunson <131382715+andreweunson@users.noreply.github.com>
2024-12-05 11:03:18 -0800

    Integrated database support

---
+++ EShoppingAppv2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EShoppingAppv2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
